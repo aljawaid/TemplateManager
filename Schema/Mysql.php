16a43a30ea292676aabc2760994cc9e71b2b788d
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-07 19:47:34 +0000

    Add database files
    - #2

---
+++ Schema/Mysql.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kanboard\Plugin\TemplateManager\Schema;
+
+use PDO;
+
+const VERSION = 1;
+
+function version_1(PDO $pdo)
+{
+    $pdo->exec('CREATE TABLE IF NOT EXISTS comment_templates (
+        id INT NOT NULL AUTO_INCREMENT,
+        project_id INT NOT NULL,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE CASCADE,
+        PRIMARY KEY(id)
+    ) ENGINE = InnoDB CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci');
+}
