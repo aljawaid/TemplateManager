a2fb603bc212377ce43e6d29ec2106939d157bd4
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-08 12:16:39 +0000

    Revert "Change database description column name"
    This reverts commit 9aed6a2cc1e142faf15e416be4875e5c3d18ec3b.

--- Template/comment_template/edit.php
+++ Template/comment_template/edit.php
@@ -7,8 +7,8 @@
     <?= $this->form->label(t('Title'), 'title') ?>
     <?= $this->form->text('title', $values, $errors, array('autofocus', 'required', 'tabindex="1"')) ?>
 
-    <?= $this->form->label(t('Template Content'), 'template_content') ?>
-    <?= $this->form->textEditor('template_content', $values, $errors, array('tabindex' => 2)) ?>
+    <?= $this->form->label(t('Template Content'), 'description') ?>
+    <?= $this->form->textEditor('description', $values, $errors, array('tabindex' => 2)) ?>
 
     <?= $this->modal->submitButtons() ?>
 </form>
