7884683b487e28bd54ac99137234a5fa9ed05ed5
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-08 00:49:28 +0000

    Add content controller

---
+++ Controller/TemplateContentController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Kanboard\Plugin\TemplateManager\Controller;
+
+use Kanboard\Controller\BaseController;
+use Kanboard\Core\Plugin\Directory;
+
+/**
+ * Plugin TemplateManager
+ * Class TemplateContentController
+ * @author aljawaid
+ */
+
+class TemplateContentController extends BaseController
+{
+    public function show(array $values = array(), array $errors = array())
+    {
+        $project = $this->getProject();
+
+        $this->response->html($this->helper->layout->project('templateManager:project_predefined_content/show', array(
+            'values' => empty($values) ? $project : $values,
+            'errors' => $errors,
+            'project' => $project,
+            'predefined_task_descriptions' => $this->predefinedTaskDescriptionModel->getAll($project['id']),
+            'saved_comment_templates' => $this->taskCommentTemplateModel->getAll($project['id']),
+            'title' => t('Template Manager'),
+        )));
+    }
+
+    public function update()
+    {
+        $project = $this->getProject();
+        $values = $this->request->getValues();
+
+        $values = array(
+            'id' => $project['id'],
+            'name' => $project['name'],
+            'predefined_email_subjects' => isset($values['predefined_email_subjects']) ? $values['predefined_email_subjects'] : '',
+        );
+
+        list($valid, $errors) = $this->projectValidator->validateModification($values);
+
+        if ($valid) {
+            if ($this->projectModel->update($values)) {
+                $this->flash->success(t('Project updated successfully.'));
+                return $this->response->redirect($this->helper->url->to('TemplateContentController', 'show', array('project_id' => $project['id'])), true);
+            } else {
+                $this->flash->failure(t('Unable to update this project.'));
+            }
+        }
+
+        return $this->show($values, $errors);
+    }
+}
