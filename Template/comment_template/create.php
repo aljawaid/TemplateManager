b2fa3ef553632baf9854c53608e1267af93c5928
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-09 00:17:54 +0000

    Enable `autocomplete` for forms

--- Template/comment_template/create.php
+++ Template/comment_template/create.php
@@ -1,7 +1,7 @@
 <div class="page-header">
     <h2><?= t('Create Comment Template') ?></h2>
 </div>
-<form method="post" action="<?= $this->url->href('CommentTemplateController', 'save', array('project_id' => $project['id'], 'plugin' => 'TemplateManager')) ?>" autocomplete="off">
+<form method="post" action="<?= $this->url->href('CommentTemplateController', 'save', array('project_id' => $project['id'], 'plugin' => 'TemplateManager')) ?>" autocomplete="on">
     <?= $this->form->csrf() ?>
 
     <?= $this->form->label(t('Title'), 'title') ?>
--- Template/comment_template/edit.php
+++ Template/comment_template/edit.php
@@ -1,7 +1,7 @@
 <div class="page-header">
     <h2><?= t('Update Comment Template') ?></h2>
 </div>
-<form method="post" action="<?= $this->url->href('CommentTemplateController', 'update', array('project_id' => $project['id'], 'id' => $template['id'], 'plugin' => 'TemplateManager')) ?>" autocomplete="off">
+<form method="post" action="<?= $this->url->href('CommentTemplateController', 'update', array('project_id' => $project['id'], 'id' => $template['id'], 'plugin' => 'TemplateManager')) ?>" autocomplete="on">
     <?= $this->form->csrf() ?>
 
     <?= $this->form->label(t('Title'), 'title') ?>
