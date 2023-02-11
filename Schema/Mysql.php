fbd75def2b14542a5e5eb44bf1b76f4039b8872a
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-11 22:29:14 +0000

    Change database table column order

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -11,8 +11,8 @@ function version_1(PDO $pdo)
     $pdo->exec('CREATE TABLE IF NOT EXISTS comment_templates (
         id INT NOT NULL AUTO_INCREMENT,
         project_id INT NOT NULL,
-        topic TEXT,
         title TEXT NOT NULL,
+        topic TEXT,
         description TEXT NOT NULL,
         FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE CASCADE,
         PRIMARY KEY(id)
