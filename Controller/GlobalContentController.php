3729369fc30cc57413e803d47002ba4b45de608e
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-15 13:25:12 +0000

    Add functions `create` `save`

--- Controller/GlobalContentController.php
+++ Controller/GlobalContentController.php
@@ -14,4 +14,30 @@ use Kanboard\Core\Controller\PageNotFoundException;
 
 class GlobalContentController extends BaseController
 {
+    public function create(array $values = array(), array $errors = array())
+    {
+        $this->response->html($this->template->render('templateManager:global_template/create', array(
+            'values' => $values,
+            'errors' => $errors,
+        )));
+    }
+
+    public function save()
+    {
+        $values = $this->request->getValues();
+
+        list($valid, $errors) = $this->predefinedTaskDescriptionValidator->validate($values);
+
+        if ($valid) {
+            if ($this->globalTemplateModel->createGlobalTemplate($values['title'], $values['description'], $values['topic']) !== false) {
+                $this->flash->success(t('Template created successfully'));
+            } else {
+                $this->flash->failure(t('Unable to create this template'));
+            }
+
+            $this->response->redirect($this->helper->url->to('GlobalTemplateController', 'show', array('plugin' => 'TemplateManager')), true);
+        } else {
+            $this->create($values, $errors);
+        }
+    }
 }
