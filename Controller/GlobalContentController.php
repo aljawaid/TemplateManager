98dc9e2ebd1f89347cdcc1c5baa90d714eb8cb32
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-15 13:24:39 +0000

    Add controller

---
+++ Controller/GlobalContentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kanboard\Plugin\TemplateManager\Controller;
+
+use Kanboard\Controller\BaseController;
+use Kanboard\Core\Plugin\Directory;
+use Kanboard\Core\Controller\PageNotFoundException;
+
+/**
+ * Plugin TemplateManager
+ * Class GlobalContentController
+ * @author aljawaid
+ */
+
+class GlobalContentController extends BaseController
+{
+}
