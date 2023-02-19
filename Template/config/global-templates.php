f0ea5263464744529f2a34e207547db6af201de4
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-19 01:58:33 +0000

    Cleanup code

--- Template/config/global-templates.php
+++ Template/config/global-templates.php
@@ -16,7 +16,7 @@
         </h2>
     </div>
     <ul class="add-templates-bar">
-        <li class="">
+
             <li class="">
                 <div class="btn add-comment-template-btn">
                     <a id="AddGlobalTemplate" href="<?= $this->url->href('GlobalContentController', 'create', array('plugin' => 'TemplateManager'), false, '', false) ?>" class="js-modal-medium" title="<?=t('Add Template') ?>">
@@ -33,24 +33,13 @@
                     </a>
                 </div>
             </li>
-</div>
-<fieldset class="global-comments-section">
+        </ul>
+
+
         <?php if (! empty($saved_global_templates)): ?>
             <span class="count-badge"><?= count($saved_global_templates) ?></span>
         <?php endif ?>
-        <legend id="GlobalTemplates" class="">
-            <svg width="20px" height="20px" class="comment-templates-icon" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
-                <g stroke-width="0"/>
-                <g stroke-linecap="round" stroke-linejoin="round"/>
-                <g>
-                    <g>
-                        <path d="M256,0C114.609,0,0,114.609,0,256s114.609,256,256,256s256-114.609,256-256S397.391,0,256,0z M256,472 c-119.297,0-216-96.703-216-216S136.703,40,256,40s216,96.703,216,216S375.297,472,256,472z"/>
-                        <path fill="#808080" d="M310.875,254.734c-40.438,0-73.172,22.078-73.172,49.234c0,12.094,10.172,25.875,20.922,34.469L248.188,368l31.703-19.469 c9.453,2.969,19.891,4.656,30.984,4.656c40.391,0,73.125-22,73.125-49.219C384,276.812,351.266,254.734,310.875,254.734z"/>
-                        <path fill="#055D20" d="M256,144c-70.688,0-128,40.234-128,89.891c0,22.062,17.781,47.234,36.562,62.922l-18.266,53.969l55.547-35.562 c7.25,2.375,14.938,4.25,22.844,5.656c-3.234-6.344-5.25-12.938-5.25-19.125c0-35.484,40.938-64.219,91.438-64.219 c27.062,0,51.188,8.422,67.938,21.578c3.312-8,5.188-16.469,5.188-25.219C384,184.234,326.688,144,256,144z"/>
-                    </g>
-                </g>
-            </svg> <?= t('Global Templates') ?>
-        </legend>
+
         <p class="section-intro">
             <?= t('These templates are used as comments for each task in each project. In the comments section of each task, a templates bar will show listing all the associated comment templates which are available to the task. Topics can help group and label similar templates.') ?>
         </p>
@@ -108,4 +97,5 @@
         <?php else: ?>
             <span class="no-templates"><?= t('No templates') ?></span>
         <?php endif ?>
-    </fieldset>
+
+</div>
