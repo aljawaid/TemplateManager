c910d4674b7edc604422a1e2e6944e7c8ad61708
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-21 05:22:31 +0000

    Add variable for frontend

--- Template/task/global-templates.php
+++ Template/task/global-templates.php
@@ -1,3 +1,6 @@
+<?php
+$saved_global_templates = $this->model->globalTemplateModel->getAll();
+?>
 <details class="accordion-section">
     <summary class="accordion-title acc-templates-title">
         <i class="fa fa-code"></i> Templates & Actions
