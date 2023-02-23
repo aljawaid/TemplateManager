280ecbd60cd28d9389cbed9469737a2e15240ee5
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-23 01:23:59 +0000

    Translate controllers

--- Locale/translations-starter-template.php
+++ Locale/translations-starter-template.php
@@ -15,6 +15,14 @@ return array(
     //
     'Predefined Contents is replaced by the Template Manager. Improve consistency of your project data and save time for repetitive comments using saved templates. Project Editors can create, update and delete templates for each project. Templates can be created for task descriptions (core feature), comments (new feature), global templates (new feature) and email subjects (core feature) all in one place including adding notes and instructions separate to the template content to help keep content neat. The global templates feature shows for every project and can be used as a way to guide and advise users within tasks.  Templates can also be useful in environments where auditing is required.' => '',
     //
+    // Controller/CommentTemplateController.php
+    //
+    'Template created successfully' => '',
+    'Unable to create this template' => '',
+    'Template updated successfully' => '',
+    'Unable to update this template' => '',
+    'Template deleted successfully' => '',
+    'Unable to delete this template' => '',
     // CORE OVERRIDES OR 3RD PARTY PLUGIN TRANSLATIONS AFFECTING THIS PLUGIN
     //
     '' => '',
