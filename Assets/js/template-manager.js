c2b7be9478dd9e753cde5a62369e43544fbd8ec3
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-24 02:13:10 +0000

    Add JS Code
    - WIP #10

--- Assets/js/template-manager.js
+++ Assets/js/template-manager.js
@@ -51,3 +51,21 @@ $( document ).ready(function() {
         }, 3500);
     });
 });
+
+KB.onClick('.js-template-title', function (e) {
+    var template = KB.dom(e.target).data('template');
+    var target = KB.dom(e.target).data('templateTarget');
+    var targetField = KB.find(target);
+    var template2 = KB.dom(e.target).data('templatetitle');
+    var target2 = KB.dom(e.target).data('templatetitleTarget');
+    var targetField2 = KB.find(target2);
+
+    if (targetField) {
+        targetField.build().value = template;
+    }
+    if (targetField2) {
+        targetField2.build().value = template2;
+    }
+
+    _KB.controllers.Dropdown.close();
+});
