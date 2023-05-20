895d013dc8b126dc32f4dcf7de973532d8c59afc
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-20 02:13:20 +0100

    FIX: Code Syntax - `TemplateTitleHelper.php`

--- Helper/TemplateTitleHelper.php
+++ Helper/TemplateTitleHelper.php
@@ -14,23 +14,23 @@ use Kanboard\Core\Base;
  */
 class TemplateTitleHelper extends TaskHelper
 {
-        public function renderDescriptionAndTitleTemplateDropdown()
+    public function renderDescriptionAndTitleTemplateDropdown()
     {
         $projectId = $this->request->getIntegerParam('project_id');
         $templates = $this->taskDescriptionTemplateModel->getAll($projectId);
 
-        if (! empty($templates)) {
+        if (!empty($templates)) {
             $html = '<div id="TaskDescDropdownWrapper" class="dropdown">';
-            $html .= '<a href="#"  id="TaskDescDropdown" class="dropdown-menu dropdown-menu-link-icon" title="' .t('Use a task description template') .'"><span class="template-manager-icon-dropdown" aria-hidden="true"></span> <span class="caret-dropdown" aria-hidden="true"></span></a>';
+            $html .= '<a href="#"  id="TaskDescDropdown" class="dropdown-menu dropdown-menu-link-icon" title="' . t('Use a task description template') . '"><span class="template-manager-icon-dropdown" aria-hidden="true"></span> <span class="caret-dropdown" aria-hidden="true"></span></a>';
             $html .= '<ul class="task-desc-template-list">';
-            $html .= '<span class="template-list-title-tm"><span class="task-desc-icon-dropdown" aria-hidden="true"></span> '. t('Task Description Templates') .'</span>';
-            $html .= '<span class="template-list-title">'. t('Templates available for this project') .'</span>';
+            $html .= '<span class="template-list-title-tm"><span class="task-desc-icon-dropdown" aria-hidden="true"></span> ' . t('Task Description Templates') . '</span>';
+            $html .= '<span class="template-list-title">' . t('Templates available for this project') . '</span>';
 
-            foreach ($templates as  $template) {
-                $html .= '<li id="TaskTemplate-'. $this->helper->text->e($template['id']) .'" class="predefined-template-item">';
-                $html .= '<a href="#" data-template-target="textarea[name=description]" data-template="'. $this->helper->text->e($template['description']) .'" data-templatetitle-target="input[name=title]" data-templatetitle="'. $this->helper->text->e($template['title']) .'" class="js-template-title" ';
+            foreach ($templates as $template) {
+                $html .= '<li id="TaskTemplate-' . $this->helper->text->e($template['id']) . '" class="predefined-template-item">';
+                $html .= '<a href="#" data-template-target="textarea[name=description]" data-template="' . $this->helper->text->e($template['description']) . '" data-templatetitle-target="input[name=title]" data-templatetitle="' . $this->helper->text->e($template['title']) . '" class="js-template-title" ';
                 if (!empty($template['note'])) {
-                    $html .= 'data-tooltip="'. $this->helper->text->e($template['note']) .'"';
+                    $html .= 'data-tooltip="' . $this->helper->text->e($template['note']) . '"';
                 };
                 $html .= '>';
                 $html .= $this->helper->text->e($template['title']);
@@ -44,5 +44,4 @@ class TemplateTitleHelper extends TaskHelper
 
         return '';
     }
-    
 }
