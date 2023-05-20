7fd1e060549117336d811a4987f649e7e7fa5a2a
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-20 02:32:55 +0100

    FIX: Code Syntax - `dropdown.php`

--- Template/project_header/dropdown.php
+++ Template/project_header/dropdown.php
@@ -1,10 +1,11 @@
-<?php
-$taskTemplatesCount =  $this->task->taskDescriptionTemplateModel->getAll($project['id']);
+<?php // phpcs:disable PSR12.Files.FileHeader.SpacingAfterBlock
+$taskTemplatesCount = $this->task->taskDescriptionTemplateModel->getAll($project['id']);
 $taskTemplatesCountTotal = count($taskTemplatesCount);
 $commentTemplatesCount = $this->task->taskCommentTemplateModel->getAll($project['id']);
 $commentTemplatesCountTotal = count($commentTemplatesCount);
 $emailSubjectsCount = (!empty($project['predefined_email_subjects']) && !is_null($project['predefined_email_subjects'])) ? count(explode("\r\n", trim($project['predefined_email_subjects']))) : 0;
 $allTemplatesCount = ($taskTemplatesCountTotal + $commentTemplatesCountTotal + $emailSubjectsCount);
+// phpcs:enable
 ?>
 
 <li <?= $this->app->checkMenuSelection('TemplateContentController') ?>>
@@ -19,5 +20,5 @@ $allTemplatesCount = ($taskTemplatesCountTotal + $commentTemplatesCountTotal + $
                 <path d="M13 15H21V17H13V15Z" fill="#0DB388"/>
                 <path d="M21 19H13V21H21V19Z" fill="#0DB388"/>
             </g>
-        </svg>'. t('Template Manager').'<span class="templates-menu-count">'. $allTemplatesCount .'</span>', 'TemplateContentController', 'show', array('project_id' => $project['id'], 'plugin' => 'TemplateManager'), false, 'template-manager-menu') ?>
+        </svg>' . t('Template Manager') . '<span class="templates-menu-count">' . $allTemplatesCount . '</span>', 'TemplateContentController', 'show', array('project_id' => $project['id'], 'plugin' => 'TemplateManager'), false, 'template-manager-menu') ?>
 </li>
