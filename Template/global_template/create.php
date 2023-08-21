8494a266ff377b78a58e1e4cf66a42e39ed08ac6
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-21 23:25:12 +0100

    FIX: Modal Title Styling Conflict
    - affected display of third party modals

--- Template/global_template/create.php
+++ Template/global_template/create.php
@@ -1,4 +1,4 @@
-<div class="modal-page-header">
+<div class="modal-page-header template-manager-modal">
     <h2 class="relative">
         <svg width="24px" height="24px" class="template-manager-icon" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <g stroke-width="0"/>
