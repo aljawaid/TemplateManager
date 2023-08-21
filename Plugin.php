315c19ed85270058cff8cd78778676a8f2684eca
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-21 23:27:43 +0100

    Fix comments

--- Plugin.php
+++ Plugin.php
@@ -84,8 +84,7 @@ class Plugin extends Base
 
     public function getPluginName()
     {
-        // Plugin Name MUST be identical to namespace for Plugin Directory to detect updated versions
-        // Do not translate the plugin name here
+        // Plugin Name MUST be identical to namespace for Plugin Directory to detect updated versions - do not translate the plugin name here
         return 'TemplateManager';
     }
 
@@ -106,10 +105,8 @@ class Plugin extends Base
 
     public function getCompatibleVersion()
     {
-        // Examples:
-        // >=1.0.37
-        // <1.0.37
-        // <=1.0.37
+        // Examples: '>=1.0.37' '<1.0.37' '<=1.0.37'
+        // Tested on KB v1.2.20 upto plugin v2.7.0, then KB v1.2.32+
         return '>=1.2.20';
     }
 
